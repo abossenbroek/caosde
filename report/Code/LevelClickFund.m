ef410a5064eecd38bc3b149ba54139bce9aa14e6
Anton Bossenbroek <anton@re-uva-91.wireless.uva.nl>
2008-04-29 17:19:06 +0200

    Fixed some minor bugs which caused code to not even run.

--- report/Code/LevelClickFund.m
+++ report/Code/LevelClickFund.m
@@ -10,12 +10,12 @@ walk2 = randn(N);
 
 fund = Portfolio;
 fund.numberOfOptions = initialStocks;
-fund.numberOfStocks = intialStock;
+fund.numberOfStocks = initialStocks;
 fund.lastStockPrice = S0;
 fund.level = level;
 
 % At this point we use (1 + level) * S_0 as the strike price of the option.
-if strcmp(method, 'BM')
+if strcmp(method, 'BS')
 	% Compute the initial value of the fund.
 	fund.value = initialStocks * S0 + BlackScholes(S0, sigma, 5, ...
 		(1 + level) * S0, r, 'put');
@@ -26,10 +26,12 @@ if strcmp(method, 'BM')
 		if (fund.lastStockPrice * (1 + fund.level)) < stock(i)
 			% Compute the profit we can make by selling the current options. 
 			currentOptionsValue = fund.numberOfOptions * ...
-				BlackScholes(fund.lastStockPrice, sigma, 5 - fund.lastChange * ...
-				dt, (1 + fund.level) * fund.lastStockPrice, r, 'put');
+				BlackScholes(fund.lastStockPrice, sigma, 5 - fund.lastChange, ...
+				(1 + fund.level) * fund.lastStockPrice, r, 'put');
 
-			numberOf
+			% Compute the price of options which will have to be bought.
+			newOptionPrice = BlackScholes(stock(i), sigma, 5 - i * dt, ...
+				(1 + fund.level) * stock(i), r, 'put');
 		end
 	end
 
