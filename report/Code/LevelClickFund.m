5c055a489081813758c3bc8fbd941e1a7e7ed41c
Anton Bossenbroek <anton@anton-bossenbroek.local>
2008-04-29 19:03:29 +0200

    Added the Black Scholes implementation of the Level-Click Fund.

--- report/Code/LevelClickFund.m
+++ report/Code/LevelClickFund.m
@@ -14,11 +14,10 @@ fund.numberOfStocks = initialStocks;
 fund.lastStockPrice = S0;
 fund.level = level;
 
-% At this point we use (1 + level) * S_0 as the strike price of the option.
 if strcmp(method, 'BS')
 	% Compute the initial value of the fund.
 	fund.value = initialStocks * S0 + BlackScholes(S0, sigma, 5, ...
-		(1 + level) * S0, r, 'put');
+		S0, r, 'put');
 	% Generate a stock path using the standard Black-Scholes model.
 	stock = BlackScholesStock(r, dt, T0, T, sigma, walk1, S0);
 
@@ -27,12 +26,42 @@ if strcmp(method, 'BS')
 			% Compute the profit we can make by selling the current options. 
 			currentOptionsValue = fund.numberOfOptions * ...
 				BlackScholes(fund.lastStockPrice, sigma, 5 - fund.lastChange, ...
-				(1 + fund.level) * fund.lastStockPrice, r, 'put');
+				fund.lastStockPrice, r, 'put');
 
 			% Compute the price of options which will have to be bought.
 			newOptionPrice = BlackScholes(stock(i), sigma, 5 - i * dt, ...
-				(1 + fund.level) * stock(i), r, 'put');
+				stock(i), r, 'put');
+
+			% Using this price we can compute the number of options which have to
+			% be bought.
+			quantity = (fund.numberOfStocks * stock(i) + currentOptionsValue) ...
+				/ (newOptionPrice + stock(i))
+
+			if quantity < 0
+				ME = MException('LevelClickFund:FewShares', ...
+					'Too few shares');
+				throw(ME);
+			end
+
+			if abs(newOptionPrice * quantity - stock(i) * ...
+				(fund.numberOfStocks - quantity) - currentOptionsValue) > 0.00001
+				ME = MException('LevelClickFund:invalidPor', ...
+					'Portfolio is incorrect');
+				throw(ME);
+			end
+
+			% Adjust the number of stocks.
+			fund.numberOfStocks = quantity;
+			% Set the new number of options.
+			fund.numberOfOptions = quantity;
+			fund.lastChange = i * dt;
+			fund.lastStockPrice = stock(i);
 		end
+
+		valueAtT =fund.numberOfOptions * ...
+				BlackScholes(fund.lastStockPrice, sigma, 5 - fund.lastChange, ...
+				fund.lastStockPrice, r, 'put') + stock(i) * fund.numberOfStocks;
+ 
 	end
 
 elseif strcmp(method, 'ST')
